f7e12e198cc3447e6401e9a527988f82aebdb03b
jswis <wiktoria3101>
2021-02-27 12:04:36 +0100

    creating simply classes

---
+++ Conversation.py
@@ -0,0 +1,17 @@
+class Conversation:
+    users=[]
+
+    def addUser(self,user):
+        self.users.append(user)
+
+    def countMessaes(self):
+        total=0
+        for user in self.users:
+            total+=user.messages
+        return total
+
+    def countChars(self):
+        total=0
+        for user in self.users:
+            total+=user.chars
+        return total
