f910bed562888f72ec82e344c6c60c0b4d36a905
bubus128 <wiktoria3101>
2021-02-28 14:21:44 +0100

    extracting users

--- Conversation.py
+++ Conversation.py
@@ -1,5 +1,7 @@
 class Conversation:
     users=[]    #array of users
+    titleCrop=["<title>","</title>"]
+    usersCrop=["class=\"_2lek\">","</div>"]
 
     def __init__(self, source):     #init
         self.source=source
@@ -7,16 +9,26 @@ class Conversation:
         self.readUsers()
 
     def readConv(self):
+        #reading conversation
         file = open(self.source, "r", encoding='utf-8')  # open read only
         self.content = file.read()
         file.close;
-        titleStart=self.content.find("<title>")+7
-        titleEnd=self.content.find("</title>")
+        #extracting title from content
+        titleStart=self.content.find(self.titleCrop[0])+len(self.titleCrop[0])
+        titleEnd=self.content.find(self.titleCrop[1])
         self.title=self.content[titleStart:titleEnd]
-        print(self.title)
+        #print(self.title)
 
     def readUsers(self):
-        print(self.title)
+        #extractiong users from content
+        startUsers=self.content.find(self.usersCrop[0])+len(self.usersCrop[0])
+        crop1=self.content[startUsers:]
+        endUsers=crop1.find(self.usersCrop[1])
+        users=crop1[:endUsers]
+        #extracting particular users
+        usersCroped=users.replace(' i ',', ').replace(' and ',',v').split(', ')
+        for user in usersCroped:
+            print(user)
 
     def addUser(self,user):
         self.users.append(user)
