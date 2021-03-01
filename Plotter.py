490e0e671c0dbeb449723984f40a35a43cd9bf18
bubus128 <j.swislocki@outlook.com>
2021-03-01 22:01:07 +0100

    percent displaying

--- Plotter.py
+++ Plotter.py
@@ -6,6 +6,6 @@ class Plotter:
         names=conversation.getNames()
         messages=conversation.getMessages()
         chars=conversation.getChars()
-        plt.pie(messages,labels=names)
-        plt.legend(loc='upper left',bbox_to_anchor=(1.3, 1))
+        plt.pie(messages,labels=names, autopct='%1.2f%%')
+        plt.legend(loc='upper left',bbox_to_anchor=(1.3, 1),labels=['%s, %1.2f %%' % (l, s/100) for l, s in zip(names, messages)])
         plt.show()
