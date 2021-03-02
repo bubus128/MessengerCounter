7a9a13724ad806e5f6f155424c19ee70e0823512
bubus128 <j.swislocki@outlook.com>
2021-03-02 17:11:17 +0100

    output to pdf

--- Conversation.py
+++ Conversation.py
@@ -4,26 +4,27 @@ from UsersRepo import UsersRepo
 
 
 class Conversation:
-    titleCrop=["<title>","</title>"]
-    usersCrop=["class=\"_2lek\">","</div>"]
-    messageSeparators=["<div class=\"_3-96 _2pio _2lek _2lel\">","</div><div class=\"_3-96 _2let\"><div><div></div><div>","</div>"]
+    titleCrop = ["<title>", "</title>"]
+    usersCrop = ["class=\"_2lek\">", "</div>"]
+    messageSeparators = ["<div class=\"_3-96 _2pio _2lek _2lel\">",
+                         "</div><div class=\"_3-96 _2let\"><div><div></div><div>", "</div>"]
 
-    def __init__(self, source):     #init
-        #seting sourcefile of conversation
-        self.source=source
-        #creating messageSender
+    def __init__(self, source):  # init
+        # seting sourcefile of conversation
+        self.source = source
+        # creating messageSender
         self.messageSender = MessageSender()
-        #users repo initialization
+        # users repo initialization
         self.users = UsersRepo(self.messageSender)
-        #red title and content of conversation
+        # red title and content of conversation
         self.readConv()
-        #create users of conversation
+        # create users of conversation
         self.readUsers()
-        #read messages
+        # read messages
         self.readMessages()
-        #sorting
+        # sorting
         self.users.messageSort()
-        #summary
+        # summary
         self.users.summary()
 
     def getNames(self):
@@ -36,40 +37,41 @@ class Conversation:
         return self.users.getChars()
 
     def readConv(self):
-        #reading conversation
+        # reading conversation
         file = open(self.source, "r", encoding='utf-8')  # open read only
         self.content = file.read()
-        file.close;
-        #extracting title from content
-        titleStart=self.content.find(self.titleCrop[0])+len(self.titleCrop[0])
-        titleEnd=self.content.find(self.titleCrop[1])
-        self.title=self.content[titleStart:titleEnd]
-        #print(self.title)
+        file.close()
+        # extracting title from content
+        titleStart = self.content.find(self.titleCrop[0]) + len(self.titleCrop[0])
+        titleEnd = self.content.find(self.titleCrop[1])
+        self.title = self.content[titleStart:titleEnd]
+        # print(self.title)
 
     def readUsers(self):
-        #extractiong users from content
-        startUsers=self.content.find(self.usersCrop[0])+len(self.usersCrop[0])
-        crop1=self.content[startUsers:]
-        endUsers=crop1.find(self.usersCrop[1])
-        users=crop1[:endUsers]
-        #extracting particular users
-        usersCroped=users.replace(' i ',', ').replace(' and ',',v').replace('Uczestnicy: ','').replace('Users: ','').split(', ')
+        # extractiong users from content
+        startUsers = self.content.find(self.usersCrop[0]) + len(self.usersCrop[0])
+        crop1 = self.content[startUsers:]
+        endUsers = crop1.find(self.usersCrop[1])
+        users = crop1[:endUsers]
+        # extracting particular users
+        usersCroped = users.replace(' i ', ', ').replace(' and ', ',v').replace('Uczestnicy: ', '').replace('Users: ',
+                                                                                                            '').split(
+            ', ')
         for user in usersCroped:
             self.users.addUser(user)
 
     def readMessages(self):
-        message=self.content
-        cropPoint=message.find(self.messageSeparators[0])
-        while(cropPoint>-1):
-            #find author of message
-            crop1=message.find(self.messageSeparators[1])
-            author=message[cropPoint+len(self.messageSeparators[0]):crop1]
-            message=message[crop1+len(self.messageSeparators[1]):]
-            #find content of message
-            crop2=message.find(self.messageSeparators[2])
-            content=message[:crop2]
-            self.users.messageFound(author,content)
+        test=100
+        message = self.content
+        cropPoint = message.find(self.messageSeparators[0])
+        while (cropPoint > -1 & test>0):
+            # find author of message
+            crop1 = message.find(self.messageSeparators[1])
+            author = message[cropPoint + len(self.messageSeparators[0]):crop1]
+            message = message[crop1 + len(self.messageSeparators[1]):]
+            # find content of message
+            crop2 = message.find(self.messageSeparators[2])
+            content = message[:crop2]
+            self.users.messageFound(author, content)
             cropPoint = message.find(self.messageSeparators[0])
-
-
-
+            test-=1
--- Plotter.py
+++ Plotter.py
@@ -1,14 +1,19 @@
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
 from Conversation import Conversation
 
 class Plotter:
+    outputLoc=r'output\outpu.pdf'
     def __init__(self,conversation):
         names=conversation.getNames()
         messages=conversation.getMessages()
         chars=conversation.getChars()
-        fig, (messagePlot, charPlot) = plt.subplots(1, 2)
-        messagePlot.pie(messages,labels=names, autopct='%1.2f%%')
-        messagePlot.legend(loc='upper left',bbox_to_anchor=(1.3, 1),labels=['%s, %1.2f %%' % (l, s/100) for l, s in zip(names, messages)])
-        charPlot.pie(chars,labels=names, autopct='%1.2f%%')
-        messagePlot.legend(loc='upper left', bbox_to_anchor=(1.3, 1),labels=['%s, %1.2f %%' % (l, s / 100) for l, s in zip(names, chars)])
-        plt.show()
+        with PdfPages(self.outputLoc) as pdf:
+            plt.pie(messages,labels=names, autopct='%1.2f%%')
+            plt.legend(loc='upper left',bbox_to_anchor=(1.3, 1),labels=['%s, %1.2f %%' % (l, s/100) for l, s in zip(names, messages)])
+            pdf.savefig()
+            plt.close()
+            plt.pie(chars,labels=names, autopct='%1.2f%%')
+            plt.legend(loc='upper left', bbox_to_anchor=(1.3, 1),labels=['%s, %1.2f %%' % (l, s / 100) for l, s in zip(names, chars)])
+            pdf.savefig()
