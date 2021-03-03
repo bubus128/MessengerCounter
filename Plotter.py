861dc2b4afa052cc782eabf0033afbd7373b115a
bubus128 <j.swislocki@outlook.com>
2021-03-03 17:00:34 +0100

    function to charts creating added

--- Plotter.py
+++ Plotter.py
@@ -10,18 +10,19 @@ class Plotter:
         totalChars=conversation.usersRepo.totalChars()
         totalMessages=conversation.usersRepo.totalMessaes()
         with PdfPages(self.outputLoc) as pdf:
-            plt.figure(figsize=[11.69,8.27])
-            plt.pie(messages,labels=names, autopct='%1.2f%%')
-            plt.title("procentowy udział względem wysłanych wiadomości").set_ha("left")
-            plt.legend(loc='upper left',bbox_to_anchor=(1.3, 1),labels=['%s, %1.2f %%' % (l, s/totalMessages*100) for l, s in zip(names, messages)])
-            plt.subplots_adjust(left=0.1, right=0.55)
+            self.chartCreate("procentowy udział względem wysłanych wiadomości",messages,names,totalMessages)
             pdf.savefig()
             plt.close()
 
-            plt.figure(figsize=[11.69, 8.27])
-            plt.pie(chars,labels=names, autopct='%1.2f%%')
-            plt.title("procentowy udział względem wysłanych znaków").set_ha("left")
-            plt.legend(loc='upper left', bbox_to_anchor=(1.3, 1),labels=['%s, %1.2f %%' % (l, s/totalChars*100) for l, s in zip(names, chars)])
-            plt.subplots_adjust(left=0.1, right=0.55)
+            self.chartCreate("procentowy udział względem wysłanych znaków", chars, names, totalChars)
             pdf.savefig()
             plt.close()
+
+
+    def chartCreate(self,title,values,lables,maxValue):
+        plt.figure(figsize=[11.69, 8.27])
+        plt.pie(values, labels=lables, autopct='%1.2f%%')
+        plt.title(title).set_ha("left")
+        plt.legend(loc='upper left', bbox_to_anchor=(1.3, 1),
+                   labels=['%s, %1.2f %%' % (l, s / maxValue * 100) for l, s in zip(lables, values)])
+        plt.subplots_adjust(left=0.1, right=0.55)
